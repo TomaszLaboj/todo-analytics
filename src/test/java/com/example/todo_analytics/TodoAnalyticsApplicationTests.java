e1ab782876b08805f3fbb613a7fde70b2337c79b
Tomasz Laboj <tomasz.laboj@ocado.com>
2026-02-27 18:34:35 +0000

    update

---
+++ src/test/java/com/example/todo_analytics/TodoAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
